c7350eab8f16a55c112d17590f073c8058275241
bella <theboss@ciaobella.me>
2021-06-23 13:40:37 -0700

    first commit

---
+++ thingy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace thingy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = Temp.FahrenheitToCelsius(68);
+            var fahrenheit = Temp.CelsiusToFarenheit(20);
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+
+        }
+    }
+}
---
+++ thingy/temp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace thingy
+{
+    public static class Temp
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var result = (fahrenheit - 32) / 1.8;
+            return result;
+        }
+        public static double CelsiusToFarenheit(double celsius)
+        {
+            var answer = (celsius * 9) / 5 + 32;
+            return answer;
+        }
+    }
+
+
+
+
+
+
+
+}
